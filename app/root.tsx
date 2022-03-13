196ba9e73e7b8fb88c3a26679269a08503b26364
Sabin Adams <sabintheworld@gmail.com>
2022-03-13 01:44:28 -0800

    Updated some metadata and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => [
 ];
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Valley Dev" };
 };
 
 export default function App() {
