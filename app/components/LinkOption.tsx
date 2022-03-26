26ed7919d156ec68f3c327c4ab626a5200456215
Sabin Adams <sabintheworld@gmail.com>
2022-03-25 19:17:17 -0700

    Button color

--- app/components/LinkOption.tsx
+++ app/components/LinkOption.tsx
@@ -6,5 +6,5 @@ interface Props {
 }
 
 export function LinkOption(props: Props) {
-    return <props.element className="transition duration-300 ease-in-out tranform hover:-translate-y-1 hover:text-emerald-700 cursor-pointer" onClick={props.onClick ?? (() => { })} />
+    return <props.element className="transition duration-300 ease-in-out tranform hover:-translate-y-1 hover:text-emerald-300 cursor-pointer" onClick={props.onClick ?? (() => { })} />
 }
